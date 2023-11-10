903c0bff72e9b04d67e9ee288d9c7223eb8d6a43
mathursrus <sid.mathur@gmail.com>
2023-11-10 11:50:52 -0800

    undefined error fix

--- src/SubmitTime.js
+++ src/SubmitTime.js
@@ -297,7 +297,7 @@
             console.log("Flyout state is ", flyoutState); 
         }, [flyoutState]); // Run the effect only when flyoutState changes
 
-        const user_circles = localStorage.getItem('userDetails') ? (JSON.parse(localStorage.getItem('userDetails'))).circles : [];
+        const user_circles = (localStorage.getItem('userDetails') !== undefined && localStorage.getItem('userDetails') !== null) ? (JSON.parse(localStorage.getItem('userDetails'))).circles : [];
 
         return (
             <Container className="p-3">    
