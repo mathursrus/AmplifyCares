6de90f368bbf269b92bbcaac0c59faff7b786cf4
mathursrus <sid.mathur@gmail.com>
2023-12-28 00:52:29 -0800

    Add additional static file serving

--- server/app.js
+++ server/app.js
@@ -80,6 +80,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Handle OPTIONS requests for writeFeedback endpoint
 app.options('/writeFeedback', (req, res) => {
